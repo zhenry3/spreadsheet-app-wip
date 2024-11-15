654778fecd09255d169ffd205698932b70233ded
zhenry3 <zhenry3@gmu.edu>
2024-11-14 23:25:10 -0500

    Add files via upload

--- cells.js
+++ cells.js
@@ -3,42 +3,30 @@ var Cells = {
     cellDoms: document.getElementsByClassName("cell"),
     cellContentDoms: document.getElementsByClassName("cell_content"),
     cellInputDoms: document.getElementsByClassName("cell_input"),
+    cellTableHeader: document.getElementsByClassName("table_header"),
 
     clickedDom: null,
-    clickedID: null,    // Starts at 1
+    clickedID: null,
 
     /**
      * Adds event listner to all cells, should run on init and spreadsheet updates
-     * @param {Boolean} all Boolean flag. Use cellDoms to determine number of cells in table.
-     * @param {Number} width 
-     * @param {Number} height 
      */
-    initCells: function (width, height) {
-        if (width == null || height == null) {
-            cell_num = Cells.cellDoms.length;
-        } else {
-            cell_num = width * height;
-        }
-
-        if (cell_num == 0) {
-            cell_num = Cells.cellDoms.length;
-        }
-        //cell_num = (width * height) ?? Cells.cellDoms.length;
-        console.log(cell_num)
+    initCells: function () {
+        cell_num = Cells.cellDoms.length;
 
         for (let i = 0; i < cell_num; i++) {
-            //cellDomArr = Array.from(this.cellDoms);
-            this.cellDoms[i].setAttribute("onclick", "Cells.clicked(" + Number(i+1) + ")");
+            this.cellDoms[i].setAttribute("onclick", "Cells.clicked(" + Number(i+1) + ");");
+            this.cellDoms[i].setAttribute("onkeydown", "Cells.arrowKeyNavigation(event);");
+            this.cellDoms[i].getElementsByClassName("cell_input")[0].setAttribute("onblur", "Cells.clearClicked();");
             this.cellInputDoms[i].style.display = "none";
         }
+
+        Array.from(Cells.cellTableHeader).forEach(function (valu, indx, arr) {
+            valu.setAttribute("onclick", "Cells.clearClicked();");
+        });
     },
 
     clicked: function (cellID) {
-        console.log("clicked");
-        //cellID = event.srcElement.cellIndex;
-
-        console.log(cellID);
-
         if (Cells.clickedDom !== null) {
             Cells.clickedDom.getElementsByClassName("cell_content")[0].innerHTML = Cells.clickedDom.getElementsByClassName("cell_input")[0].value;
         }
@@ -52,7 +40,6 @@ var Cells = {
     click: function (cellID) {
         Cells.clearClicked();
         var cell = Cells.cellDoms[cellID -1];
-        console.log(cell)
 
         cell.getElementsByClassName("cell_content")[0].style.display = "none";
         cell.getElementsByClassName("cell_input")[0].style.display = "block";
@@ -61,10 +48,41 @@ var Cells = {
     },
 
     clearClicked: function () {
+        if (Cells.clearClicked != null) {
+            Cells.clickedDom.getElementsByClassName("cell_content")[0].innerHTML = Cells.clickedDom.getElementsByClassName("cell_input")[0].value;
+        }
+
         Array.from(Cells.cellDoms).forEach(function (valu, indx, arr) {
+            valu.getElementsByClassName("cell_content").innerHTML = valu.getElementsByClassName("cell_content").value;
             valu.getElementsByClassName("cell_content")[0].style.display = "block";
             valu.getElementsByClassName("cell_input")[0].style.display = "none";
             valu.style.border = "1px solid black";
         });
+    },
+
+    arrowKeyNavigation: function (event) {
+        switch (event.keyCode) {
+            case 38:    // Up arrow
+                if (Cells.clickedID > 26) {
+                    this.clicked(Cells.clickedID - 26);
+                }
+                break;
+            case 40:    // Down arrow
+            case 13:    // or enter
+                if (Cells.clickedID < ((Rows.rows * 26) - 26)) {
+                    this.clicked(Cells.clickedID + 26);
+                }
+                break;
+            case 37:    // Left arrow
+                if (Cells.clickedID > 1) {
+                    this.clicked(Cells.clickedID - 1);
+                }
+                break;
+            case 39:    // Right arrow
+                if (Cells.clickedID < Cells.cellDoms.length) {
+                    this.clicked(Cells.clickedID + 1);
+                }
+                break;
+        }
     }
 }
